91d51d2a7a9d35831279828604d314685636e455
Nishant_mac <nishant.gadhvi@gmail.com>
2019-06-01 16:47:52 -0500

    Data prep complete. Added helper file

---
+++ helpers/helper.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def merge_data(feature, target):
+    """Merge the features and label datasets. Both need to have a common key `jobId`
+
+    Keyword arguments:
+    feature -- the dataframe of job description
+    target -- the dataframe of salaries
+    
+    Returns:
+    a merged pandas dataframe
+    """
+    merged_df = pd.merge(feature, target, on='jobId')
+    return merged_df
